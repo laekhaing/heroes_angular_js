12dd3bc3528ec784819c993ac1e7a892583fce26
laekhaing <laelaekhaing105@gmail.com>
2018-12-14 09:30:24 +0900

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero [] = [
+  { id: 1, name: 'Mr. Naing Ye Lin'},
+  { id: 2, name: 'Mr. Moe Arkar'},
+  { id: 3, name: 'Mr. Si Thu Naing'},
+  { id: 4, name: 'Mr. La Won Maung'},
+  { id: 5, name: 'Mr. Myat Lynn Maung'},
+  { id: 6, name: 'Mr. Lynn Myat Maung'},
+  { id: 7, name: 'Ms. Myat Muu Haethi'}
+];
